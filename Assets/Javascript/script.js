ff1366475995558ac4bca3778304aa22e0b9f554
Saif <raosaif786pak@gmail.com>
2024-03-13 02:36:50 +0500

    Added more sections

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -471,11 +471,123 @@ function canvas() {
 
         trigger: "canvas",
         pin: true,
-        // markers:true,
         scroller: 'main',
-        //   set start end according to preference
         start: 'top top',
         end: '600% top',
     });
 }
 canvas();
+
+/*
+function canvas1() {
+    const canvas = document.querySelector(".page-13>canvas");
+    const context = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+
+    window.addEventListener("resize", function () {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        render();
+    });
+
+    function files(index) {
+        var data1 = `
+  ../Img/Vision00001.png
+  ../Img/Vision00002.png
+  ../Img/Vision00003.png
+  ../Img/Vision00004.png
+  ../Img/Vision00005.png
+  ../Img/Vision00006.png
+  ../Img/Vision00007.png
+  ../Img/Vision00008.png
+  ../Img/Vision00009.png
+  ../Img/Vision00010.png
+  ../Img/Vision00011.png
+  ../Img/Vision00012.png
+  ../Img/Vision00013.png
+  ../Img/Vision00014.png
+  ../Img/Vision00015.png
+  ../Img/Vision00016.png
+  ../Img/Vision00017.png
+  ../Img/Vision00018.png
+  ../Img/Vision00019.png
+  ../Img/Vision00020.png
+  ../Img/Vision00021.png
+  ../Img/Vision00022.png
+  ../Img/Vision00023.png
+  ../Img/Vision00024.png
+  ../Img/Vision00025.png
+  `;
+        return data1.split("\n")[index];
+    }
+
+    const frameCount = 25;
+
+    const images = [];
+    const imageSeq = {
+        frame: 1,
+    };
+
+    for (let i = 0; i < frameCount; i++) {
+        const img = new Image();
+        img.src = files(i);
+        images.push(img);
+    }
+
+    gsap.to(imageSeq, {
+        frame: frameCount - 1,
+        snap: "frame",
+        ease: 'none',
+        scrollTrigger: {
+            scrub: 0.15,
+            trigger: '.page-13',
+            //   set start end according to preference
+            start: 'top top',
+            end: '80% top',
+            scroller: 'main',
+        },
+        onUpdate: render,
+    });
+
+    images[1].onload = render;
+
+    function render() {
+        scaleImage(images[imageSeq.frame], context);
+    }
+
+    function scaleImage(img, ctx) {
+        var canvas = ctx.canvas;
+        var hRatio = canvas.width / img.width;
+        var vRatio = canvas.height / img.height;
+        var ratio = Math.max(hRatio, vRatio);
+        var centerShift_x = (canvas.width - img.width * ratio) / 2;
+        var centerShift_y = (canvas.height - img.height * ratio) / 2;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(
+            img,
+            0,
+            0,
+            img.width,
+            img.height,
+            centerShift_x,
+            centerShift_y,
+            img.width * ratio,
+            img.height * ratio
+        );
+    }
+    ScrollTrigger.create({
+
+        trigger: ".page-13",
+        pin: true,
+        // markers:true,
+        scroller: 'main',
+        //   set start end according to preference
+        start: 'top top',
+        end: '80% top',
+    });
+}
+canvas1();
+*/
